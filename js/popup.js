d945c29bd00b1d56864734c1f2f68d14fe9b6360
Jeremy Seago <seagoj@gmail.com>
2016-07-20 09:44:53 -0500

    updates to ES2015 syntax

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,6 @@
-chrome.tabs.getSelected(null, function(tab) {
+chrome.tabs.getSelected(null, (tab) => {
   chrome.tabs.update(
-    { url: 'http://fuckyeahmarkdown.com/go/?showframe=0&u=' + tab.url },
-    function() { window.close(); }
+    { url: 'http://fuckyeahmarkdown.com/go/?showframe=0&read=1&md=1&u=' + tab.url },
+    () => { window.close(); }
   );
 });
